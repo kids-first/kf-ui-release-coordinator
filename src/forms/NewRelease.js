2bd128e3c837875b85794446fcf786ff16c8ca05
Dan Kolbman <dankolbman@gmail.com>
2018-04-10 15:04:58 -0400

    :sparkles: Connect release planner to api

--- src/forms/NewRelease.js
+++ src/forms/NewRelease.js
@@ -1,38 +1,67 @@
 import React, { Component } from 'react';
-import { Input, Button, Form, Transfer } from 'antd';
+import { withRouter } from "react-router-dom";
+import axios from 'axios';
+import { Alert, Col, Input, Button, Form, Row, Select, Transfer } from 'antd';
 const FormItem = Form.Item;
+const Option = Select.Option;
 
 class NewReleaseForm extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
 
-    let data = [];
-    for (let i = 0; i < 20; i++) {
-      const study = {
-        key: i.toString(),
-        title: `ST_${i + 1}`,
-        description: `Seidman study ${i + 1}`,
-        chosen: false
-      };
-      data.push(study);
-    }
+    let options = [
+      'Ready for Publication',
+      'Under Review',
+      'Returned for Modifications',
+      'For Immediate Release',
+      'Redacted'
+    ].map((el, i) => <Option key={i}>{el}</Option>);
 
     this.state = {
-      data: data,
-			targetKeys: []
+      data: [],
+			targetKeys: [],
+      loading: true,
+      tags: [],
+      options: options,
+      error: ''
 		}
-    console.log(process.env.REACT_APP_DATASERVICE_API);
   }
 
-  onComponentDidMount() {
+  componentDidMount() {
+    let api = process.env.REACT_APP_DATASERVICE_API;
+    axios.get(`${api}/studies?size=100`)
+      .then(resp => {
+        let studies = resp.data.results.map((s, i) => ({
+          key: s.kf_id,
+          title: s.kf_id,
+          description: `${s.name}`,
+          chosen: false
+        }));
+        this.setState({data: studies, loading: false, error: '', tags: []});
+      });
   }
 
   handleSubmit = (e) => {
+    let api = process.env.REACT_APP_COORDINATOR_API;
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        let release = {
+          name: values.title,
+          description: values.description,
+          studies: this.state.targetKeys,
+          tags: this.state.tags.map(tag => tag.label)
+        };
+        this.setState({loading: true});
+        axios.post(`${api}/releases`, release)
+          .then(resp => {
+            this.props.history.push(`/releases/${resp.data.kf_id}`);
+          })
+          .catch( err => {
+            console.log(err);
+            this.setState({loading: false, error: JSON.stringify(err.response.data.results)});
+          });
       }
     });
   }
@@ -44,55 +73,89 @@ class NewReleaseForm extends Component {
 	handleChange = (targetKeys) => {
     this.setState({ targetKeys });
   }
+  
+	handleTags = (tags) => {
+    this.setState({ tags: tags });
+  }
 
   renderItem = (item) => {
     const customLabel = (
-      <span className="custom-item">
+      <span>
         {item.title} - {item.description}
       </span>
     );
 
     return {
-      label: customLabel, // for displayed item
-      value: item.title, // for title and filter matching
+      label: customLabel,
+      value: item.title,
     };
   }
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
       <Form onSubmit={this.handleSubmit}>
-        <FormItem label="Release Title">
-          {getFieldDecorator('title', {
-            rules: [{ required: true, message: 'Please provide a title!' }],
-          })(
-            <Input placeholder="Title" />
-          )}
-        </FormItem>
-        <FormItem label="Release Notes">
-          {getFieldDecorator('description', {
-            rules: [{ required: false, message: 'Please provide a title!' }],
-          })(
-            <Input.TextArea placeholder="Provide release notes here" />
-          )}
-        </FormItem>
-        <FormItem label="Studies">
-          <Transfer
-						className='picker'
-            dataSource={this.state.data}
-            showSearch
-            listStyle={{
-              width: '45%',
-              height: 300
-            }}
-            titles={['Available Studies', 'Studies Added to Release']}
-            filterOption={this.filterOption}
-            targetKeys={this.state.targetKeys}
-            onChange={this.handleChange}
-            render={this.renderItem}
-          />
-        </FormItem>
+        <Row gutter={8} type='flex' justify='space-around'>
+          <Col span={12}>
+            <FormItem label="Release Title">
+              {getFieldDecorator('title', {
+                rules: [{ required: true, message: 'Please provide a title!' }],
+              })(
+                <Input placeholder="Title" />
+              )}
+            </FormItem>
+          </Col>
+          <Col span={12}>
+            <FormItem label='Tags'>
+              <Select
+                classname='select'
+                mode="tags"
+                placeholder="tags"
+                defaultValue={[]}
+                onChange={this.handleTags}
+                labelInValue
+              >
+              {this.state.options}
+              </Select>
+            </FormItem>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <FormItem label="Release Notes">
+              {getFieldDecorator('description', {
+                rules: [{ required: false, message: 'Please provide a title!' }],
+              })(
+                <Input.TextArea placeholder="Provide release notes here" />
+              )}
+            </FormItem>
+          </Col>
+        </Row>
+        <Row type='flex' justify='space-around'>
+          <Col span={24}>
+            <FormItem label="Studies">
+              <Transfer
+                className='picker'
+                dataSource={this.state.data}
+                showSearch
+                listStyle={{
+                  width: '48%',
+                  height: 300
+                }}
+                notFoundContent='No studies added to the release yet'
+                titles={['Available Studies', 'Studies Added to Release']}
+                filterOption={this.filterOption}
+                targetKeys={this.state.targetKeys}
+                onChange={this.handleChange}
+                render={this.renderItem}
+              />
+            </FormItem>
+          </Col>
+        </Row>
+        {this.state.error && (
+          <Alert type='error' message={this.state.error} />
+        )}
         <FormItem>
-          <Button type="primary" htmlType="submit">
+          <Button type="primary" loading={this.state.loading} htmlType="submit">
             Start Release 
           </Button>
         </FormItem>
@@ -103,4 +166,4 @@ class NewReleaseForm extends Component {
 
 const WrappedNewReleaseForm = Form.create()(NewReleaseForm);
 
-export default WrappedNewReleaseForm;
+export default withRouter(WrappedNewReleaseForm);
