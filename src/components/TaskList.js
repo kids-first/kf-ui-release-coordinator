5bcb302f73bfa0dc739c49f1959780713955113a
Dan Kolbman <dankolbman@gmail.com>
2018-04-18 21:21:04 -0400

    :lipstick: Prettify task list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -22,10 +22,59 @@ class TaskList extends Component {
       });
   }
 
+  whatIcon(state) {
+    var icon = '';
+    switch (state) {
+      case 'pending':
+      case 'waiting':
+        icon = 'ellipsis';
+        break;
+      case 'running':
+      case 'publishing':
+        icon = 'loading';
+        break;
+      case 'canceled':
+        icon = 'close';
+        break;
+      case 'failed':
+        icon = 'exclamation';
+        break;
+      case 'published':
+        icon = 'check';
+        break;
+    }
+    return icon;
+  }
+
+  whatColor(state) {
+    var color = '';
+    switch (state) {
+      case 'pending':
+      case 'waiting':
+        color = null;
+        break;
+      case 'running':
+      case 'publishing':
+        color = 'orange';
+        break;
+      case 'canceled':
+        color = 'magenta';
+        break;
+      case 'failed':
+        color = 'blue';
+        break;
+      case 'published':
+        color = 'green';
+        break;
+    }
+    return color;
+  }
+
   render() {
     if (this.state.loading) {
       return (<Spin tip='loading...'><Card style={{height: 300}}></Card></Spin>)
     }
+
 
     return (
         <List
@@ -34,12 +83,14 @@ class TaskList extends Component {
          renderItem={item => (
            <List.Item>
              <List.Item.Meta
-               avatar={<Avatar icon="ellipsis"/>}
+               avatar={<Avatar
+                        style={{ backgroundColor: this.whatColor(item.state)}}
+                        icon={this.whatIcon(item.state)}/>}
                title={item.kf_id}
                description={item.created_at}
              />
              <div>
-              <Tag>{item.state}</Tag>
+              <Tag color={this.whatColor(item.state)}>{item.state}</Tag>
              </div>
           </List.Item>
         )}
