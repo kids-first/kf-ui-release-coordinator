b687662b05ca199af1f1b2ee2686050a5bb9320b
Dan Kolbman <dankolbman@gmail.com>
2018-07-10 13:10:12 -0400

    :sparkles: Add enable button to service view

--- src/views/Service.js
+++ src/views/Service.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Button, Card, Divider, Input, Icon, Row, Col, Spin, Tag, Tooltip } from 'antd';
+import { Button, Card, Divider, Input, Icon, Row, Col, Spin, Tag, Switch, Tooltip } from 'antd';
 import TaskList from '../components/TaskList';
 import StatusBadge from '../components/StatusBadge';
 import Events from '../components/Events';
@@ -18,7 +18,8 @@ class Service extends Component {
       loading: true,
       service: {},
       events: [],
-      updating: false
+      updating: false,
+      toggling: false
     };
   }
 
@@ -62,6 +63,19 @@ class Service extends Component {
     this.setState({service: service})
   }
 
+  toggle(enabled) {
+    this.setState({toggling: true});
+    const token = this.props.egoToken;
+    const header = {headers: {Authorization: 'Bearer '+token}};
+    axios.patch(`${coordinatorApi}/task-services/${this.state.service.kf_id}`,
+                {enabled: enabled}, header)
+      .then(resp => {
+        let service = this.state.service;
+        service.enabled = enabled
+        this.setState({service: service, toggling: false});
+      });
+  }
+
   render() {
     if (this.state.loading) {
       return (<Spin tip='loading...'><Card style={{height: 300}}></Card></Spin>)
@@ -71,19 +85,32 @@ class Service extends Component {
       <Card title='Task Service'>
         <Row type="flex" justify="space-between">
           <Col>
-              {!this.state.editing ? (
-                <h3>{this.state.service.name}</h3>
-              ) : (
-                <Input value={this.state.service.name} onChange={ev => this.updateName(ev)}/>
-              )}
-              <Tag>{this.state.service.kf_id}</Tag>
+            {!this.state.editing ? (
+              <h3>{this.state.service.name}</h3>
+            ) : (
+              <Input value={this.state.service.name} onChange={ev => this.updateName(ev)}/>
+            )}
+            <Tag>{this.state.service.kf_id}</Tag>
             <h5><Icon type='calendar' /> Created At: <em>{Date(this.state.service.created_at)}</em></h5>
             <h5><Icon type='link' /> Endpoint: <em>{this.state.service.url}</em></h5>
             <h5><Icon type='user' /> Author: <em>{this.state.service.author}</em></h5>
           </Col>
           <Col>
-            <StatusBadge healthStatus={this.state.service.health_status} />
-            <br />
+            <Row type="flex" align="middle" gutter={16}>
+              <Col align="middle">
+                <b>Enabled: </b>
+                <Switch
+                  checkedChildren={<Icon type="check" />}
+                  unCheckedChildren={<Icon type="cross" />}
+                  checked={this.state.service.enabled}
+                  loading={this.state.toggling}
+                  onChange={(enabled) => this.toggle(enabled)}
+                  />
+              </Col>
+              <Col>
+                <StatusBadge healthStatus={this.state.service.health_status} />
+              </Col>
+            </Row>
             <br />
             {!this.state.editing ? (
               <Button type="dashed" icon="edit" size='large'
