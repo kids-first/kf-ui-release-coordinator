2c13ea7ffc40a311b205a8d93535a831ba55ba7c
Dan Kolbman <dankolbman@gmail.com>
2018-07-10 12:59:03 -0400

    :sparkles: Allow editing of service

--- src/views/Service.js
+++ src/views/Service.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Card, Divider, Icon, Row, Col, Spin, Tag, Tooltip } from 'antd';
+import { Button, Card, Divider, Input, Icon, Row, Col, Spin, Tag, Tooltip } from 'antd';
 import TaskList from '../components/TaskList';
 import StatusBadge from '../components/StatusBadge';
 import Events from '../components/Events';
 import { coordinatorApi } from '../globalConfig';
+import { UserContext } from '../contexts';
+
+const { TextArea } = Input;
 
 
 class Service extends Component {
@@ -14,7 +17,8 @@ class Service extends Component {
     this.state = {
       loading: true,
       service: {},
-      events: []
+      events: [],
+      updating: false
     };
   }
 
@@ -25,10 +29,37 @@ class Service extends Component {
             this.setState({
               service: service.data,
               events: events.data.results,
-              loading: false
+              loading: false,
+              editing: false
             });
          }))
          .catch(error => console.log(error));
+  }
+
+  edit() {
+    this.setState({editing: true});
+  }
+
+  update() {
+    this.setState({editing: false, updating: true});
+    const token = this.props.egoToken;
+    const header = {headers: {Authorization: 'Bearer '+token}};
+    axios.patch(`${coordinatorApi}/task-services/${this.state.service.kf_id}`, this.state.service, header)
+      .then(resp => {
+        this.setState({updating: false});
+      });
+  }
+
+  updateName(ev) {
+    let service = this.state.service;
+    service.name = ev.target.value
+    this.setState({service: service})
+  }
+
+  updateDescription(ev) {
+    let service = this.state.service;
+    service.description = ev.target.value
+    this.setState({service: service})
   }
 
   render() {
@@ -37,10 +68,14 @@ class Service extends Component {
     }
 
     return (
-      <Card title={`Task Service - ${this.state.service.name}`}>
+      <Card title='Task Service'>
         <Row type="flex" justify="space-between">
           <Col>
-            <h3 style={{display: "inline"}}>{this.state.service.name} </h3>
+              {!this.state.editing ? (
+                <h3>{this.state.service.name}</h3>
+              ) : (
+                <Input value={this.state.service.name} onChange={ev => this.updateName(ev)}/>
+              )}
               <Tag>{this.state.service.kf_id}</Tag>
             <h5><Icon type='calendar' /> Created At: <em>{Date(this.state.service.created_at)}</em></h5>
             <h5><Icon type='link' /> Endpoint: <em>{this.state.service.url}</em></h5>
@@ -48,13 +83,35 @@ class Service extends Component {
           </Col>
           <Col>
             <StatusBadge healthStatus={this.state.service.health_status} />
+            <br />
+            <br />
+            {!this.state.editing ? (
+              <Button type="dashed" icon="edit" size='large'
+                onClick={() => this.edit()}
+                style={{float: 'right'}}
+              >Edit</Button>
+            ) : (
+              <Button type="primary" icon="check" size='large'
+                onClick={() => this.update()}
+                style={{float: 'right'}}
+                loading={this.state.updating}
+              >Save</Button>
+            )}
           </Col>
         </Row>
         <Row>
-          <h5>Description:</h5>
+          <h5><Icon type='form' /> Description:</h5>
+          {!this.state.editing ? (
           <p>
             {this.state.service.description}
           </p>
+          ) : (
+          <p>
+            <TextArea placeholder="Autosize height based on content lines" autosize
+              value={this.state.service.description}
+              onChange={ev => this.updateDescription(ev)}/>
+          </p>
+          )}
         </Row>
 
         <Divider style={{margin: 0, marginBottom: '24px'}}/>
@@ -82,4 +139,13 @@ class Service extends Component {
   }
 }
 
-export default Service;
+function ServiceProps(props) {
+  return (
+    <UserContext.Consumer>
+      {user => <Service{...props}
+        user={user.user} egoToken={user.egoToken}/>}
+    </UserContext.Consumer>
+  )
+};
+
+export default ServiceProps;
