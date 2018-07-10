b6665aad96f49827dc6638a2542b9db4fc020808
Dan Kolbman <dankolbman@gmail.com>
2018-07-09 21:33:04 -0400

    :art: Add cards to status page

--- src/views/Status.js
+++ src/views/Status.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Alert, Col, Divider, Card, Row, Icon, Tooltip } from 'antd';
+import { Link } from 'react-router-dom';
+import { Alert, Avatar, Button, Col, Divider, Card, Row, Icon, Tooltip } from 'antd';
 import ServiceList from '../components/ServiceList';
 import Events from '../components/Events';
 import { coordinatorApi } from '../globalConfig';
 
+const { Meta } = Card;
 
 class Status extends Component {
 
@@ -36,44 +38,79 @@ class Status extends Component {
             this.setState({
               events: events.data.results,
             });
-            this.timer = setTimeout(() => this.getData(), 1000);
+            this.timer = setTimeout(() => this.getData(), 10000);
          })
          .catch(error => console.log(error));
   }
 
   render() {
     return (
-      <Card>
-        <Row style={{margin: '10px 0'}}>
+      <div>
+        <Row gutter={8} style={{margin: '8px 0'}}>
+        <Col>
           <Alert
             message="System Status"
             description="Ready to create new release"
             type="success"
             showIcon
           />
+        </Col>
         </Row>
-
-        <Divider />
-
-        <Row justify='space-around' type='flex'>
-          <Col span={10}>
-            <h2>Task Service Status <span />
-            <Tooltip title="Current state of Task Services">
-              <Icon type='info-circle-o' />
-            </Tooltip>
-            </h2>
-            <ServiceList />
+        <Row gutter={8} type='flex' style={{margin: '8px 0'}}>
+          <Col span={8}>
+            <Card style={{height: '230px'}}
+              actions={[<Link to="/service/new"><Button size='large' type='primary'>Register</Button></Link>]}>
+              <Meta
+                  avatar={<Avatar icon="tool" />}
+                  title="Register a Task Service"
+                  description="Task Services follow a common HTTP API spec to process work for a release. Register an API endpoint for a task service below."
+              />
+            </Card>
           </Col>
-          <Col span={10}>
-            <h2>Recent Release Events <span />
-            <Tooltip title="Latest events reported to the Coordinator">
-              <Icon type='info-circle-o' />
-            </Tooltip>
-            </h2>
-            <Events events={this.state.events} />
+          <Col span={8}>
+            <Card style={{height: '230px'}}
+              actions={[<Link to="/planner"><Button style={{width: '100%'}} size='large' type='primary'>Plan</Button></Link>]}>
+              <Meta
+                  avatar={<Avatar icon="calendar" />}
+                  title="Plan a Release"
+                  description="Select studies to be released and submit for review and processing."
+              />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card style={{height: '230px'}}
+              actions={[<Link to="/releases"><Button size='large' type='primary'>View</Button></Link>]}>
+              <Meta
+                  avatar={<Avatar icon="tag" />}
+                  title="View Past Releases"
+                  description="View a history and status of past releases."
+              />
+            </Card>
           </Col>
         </Row>
-      </Card>
+        <Row gutter={8} type='flex' style={{margin: '8px 0'}}>
+          <Col span={12}>
+            <Card>
+              <h2>Task Service Status <span />
+              <Tooltip title="Current state of Task Services">
+                <Icon type='info-circle-o' />
+              </Tooltip>
+              </h2>
+              <ServiceList />
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card>
+              <h2>Recent Release Events <span />
+              <Tooltip title="Latest events reported to the Coordinator">
+                <Icon type='info-circle-o' />
+              </Tooltip>
+              </h2>
+              <Events events={this.state.events} />
+            </Card>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
