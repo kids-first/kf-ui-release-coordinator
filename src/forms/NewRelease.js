397ec73de7c0a6cb7eb289115144d59dd0aa5bf1
Dan Kolbman <dankolbman@gmail.com>
2018-09-19 11:55:04 -0400

    :sparkles: Add confirmation dialog for new releases

--- src/forms/NewRelease.js
+++ src/forms/NewRelease.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
 import axios from 'axios';
 import { Alert, Col, Input, Tag, Button, Form, Row, Icon,
-  Checkbox, Select, Table } from 'antd';
+  Checkbox, Select, Table, Modal } from 'antd';
 import TimeAgo from 'react-timeago'
 import { compareSemVer } from '../utils';
 import { coordinatorApi } from '../globalConfig';
@@ -17,6 +17,7 @@ import { UserContext } from '../contexts';
 import ServiceList from '../components/ServiceList';
 const FormItem = Form.Item;
 const Option = Select.Option;
+const confirm = Modal.confirm
 
 class NewReleaseForm extends Component {
 
@@ -98,13 +99,12 @@ class NewReleaseForm extends Component {
   }
 
   handleSubmit = (e) => {
-    console.log('SEND');
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       const rawContentState = convertToRaw(this.state.editorState.getCurrentContent());
       const markdownNotes = draftToMarkdown(rawContentState);
 
-      if (!err) {
+      if (err === null) {
         let release = {
           name: values.title,
           description: markdownNotes,
@@ -113,18 +113,50 @@ class NewReleaseForm extends Component {
           is_major: values.isMajor,
           author: this.props.user.name
         };
-        const token = this.props.egoToken;
-        const header = {headers: {Authorization: 'Bearer '+token}};
 
-        this.setState({loading: true});
-        axios.post(`${coordinatorApi}/releases`, release, header)
-          .then(resp => {
-            this.props.history.push(`/releases/${resp.data.kf_id}`);
-          })
-          .catch( err => {
-            console.log(err);
-            this.setState({loading: false, error: JSON.stringify(err.response.data)});
-          });
+        let studyByKfId = {};
+
+        for(let i in this.state.data) {
+          studyByKfId[this.state.data[i].kf_id] = this.state.data[i].name;
+        }
+        const list = <ul>{release.studies.map((study) => <li>{studyByKfId[study]}</li>)}</ul>;
+        let text = (
+          <p>You are about to begin a release for {release.studies.length} studies:
+          {list}
+
+          These studies will be staged for review.
+          This <b>will not</b> affect any public facing data until it is reviewed and published.
+          </p>
+        );
+        console.log(this.state.data);
+        console.log(studyByKfId);
+        confirm({
+          title: 'Submit release for processing',
+          content: text,
+          width: '80%',
+          okText: 'Let\'s do it',
+          cancelText: 'Wait!',
+          onOk: (close) => {
+            const token = this.props.egoToken;
+            const header = {headers: {Authorization: 'Bearer '+token}};
+
+            this.setState({loading: true});
+            axios.post(`${coordinatorApi}/releases`, release, header)
+              .then(resp => {
+                this.props.history.push(`/releases/${resp.data.kf_id}`);
+                close();
+              })
+              .catch( err => {
+                console.log(err);
+                this.setState({loading: false, error: JSON.stringify(err.response.data)});
+                close();
+              });
+          },
+          onCancel(close) {
+            close();
+          },
+        });
+
       }
     });
   }
@@ -246,7 +278,11 @@ class NewReleaseForm extends Component {
           <Alert type='error' message={this.state.error} />
         )}
         <FormItem>
-          <Button type="primary" loading={this.state.loading} htmlType="submit">
+          <Button
+            type="primary"
+            loading={this.state.loading}
+            htmlType="submit"
+          >
             Start Release 
           </Button>
         </FormItem>
