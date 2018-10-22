c026840271c1461705b0b27e680d9189a35e8fe9
Dan Kolbman <dankolbman@gmail.com>
2018-10-22 10:26:49 -0400

    :rotating_light: Remove unused import

--- src/components/ServiceList.js
+++ src/components/ServiceList.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link, withRouter } from 'react-router-dom';
 import { List, Avatar, Icon, Switch } from 'antd';
-import StatusBadge from '../components/StatusBadge';
 import { UserContext } from '../contexts';
 import { coordinatorApi } from '../globalConfig';
 
