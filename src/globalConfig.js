1c8f175b336e4e58523d373fdae33e1a3600c02c
Dan Kolbman <dankolbman@gmail.com>
2018-12-13 13:35:23 -0500

    :sparkles: Add banner indicating which environment you're in

--- src/globalConfig.js
+++ src/globalConfig.js
@@ -1,5 +1,6 @@
+export const syslevel = process.env.REACT_APP_ENV;
 export const dataserviceApi = process.env.REACT_APP_DATASERVICE_API;
-export const coordinatorApi = process.env.REACT_APP_COORDINATOR_API
-export const reportsApi = process.env.REACT_APP_REPORTS_API
+export const coordinatorApi = process.env.REACT_APP_COORDINATOR_API;
+export const reportsApi = process.env.REACT_APP_REPORTS_API;
 export const egoApi = process.env.REACT_APP_EGO_API;
 export const googleAppId = process.env.REACT_APP_GOOGLE_APP_ID;
