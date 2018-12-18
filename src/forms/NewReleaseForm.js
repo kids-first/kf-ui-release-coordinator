84599656aedb2af75003fd439d84bc16c33247a3
Dan Kolbman <dankolbman@gmail.com>
2018-12-18 13:06:54 -0500

    :recycle: Refactor planner form for new releases

--- src/forms/NewReleaseForm.js
+++ src/forms/NewReleaseForm.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import axios from 'axios';
-import {Alert, Col, Tag, Row, Modal} from 'antd';
+import {Modal} from 'antd';
 import TimeAgo from 'react-timeago';
-import {Control, Form} from 'react-redux-form';
+import {Control, Form, Errors} from 'react-redux-form';
 import {Button} from 'kf-uikit';
 import {coordinatorApi} from '../globalConfig';
 import StudiesContainer from '../containers/StudiesContainer';
@@ -16,92 +17,118 @@ const confirm = Modal.confirm;
 
 class NewReleaseForm extends Component {
   handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (err === null) {
-        let release = {
-          name: values.title,
-          description: '',
-          studies: this.state.selectedRowKeys,
-          tags: this.state.tags.map(tag => tag.label),
-          is_major: values.isMajor,
-          author: this.props.user.name,
-        };
+    console.log('submitting!');
+    console.log(e);
 
-        let studyByKfId = {};
+    let release = {
+      name: e.title,
+      description: '',
+      studies: e.studies,
+      tags: [],
+      is_major: e.isMajor,
+      author: this.props.user.name,
+    };
 
-        for (let i in this.state.data) {
-          studyByKfId[this.state.data[i].kf_id] = this.state.data[i].name;
-        }
-        const list = (
-          <ul>
-            {release.studies.map(study => (
-              <li>{studyByKfId[study]}</li>
-            ))}
-          </ul>
-        );
-        let text = (
-          <p>
-            You are about to begin a release for {release.studies.length}{' '}
-            studies:
-            {list}
-            These studies will be staged for review. This <b>will not</b> affect
-            any public facing data until it is reviewed and published.
-          </p>
-        );
-        confirm({
-          title: 'Submit release for processing',
-          content: text,
-          width: '80%',
-          okText: "Let's do it",
-          cancelText: 'Wait!',
-          onOk: close => {
-            const token = this.props.egoToken;
-            const header = {headers: {Authorization: 'Bearer ' + token}};
+    let studyByKfId = {};
 
-            this.setState({loading: true});
-            axios
-              .post(`${coordinatorApi}/releases`, release, header)
-              .then(resp => {
-                this.props.history.push(`/releases/${resp.data.kf_id}`);
-                close();
-              })
-              .catch(err => {
-                console.log(err);
-                this.setState({
-                  loading: false,
-                  error: JSON.stringify(err.response.data),
-                });
-                close();
-              });
-          },
-          onCancel(close) {
+    const list = (
+      <ul>
+        {release.studies.map(study => (
+          <li key={study}>{this.props.studies[study].name}</li>
+        ))}
+      </ul>
+    );
+    let text = (
+      <p>
+        You are about to begin a release for {release.studies.length} studies:
+        {list}
+        These studies will be staged for review. This <b>will not</b> affect any
+        public facing data until it is reviewed and published.
+      </p>
+    );
+    confirm({
+      title: 'Submit release for processing',
+      content: text,
+      width: '80%',
+      okText: "Let's do it",
+      cancelText: 'Wait!',
+      onOk: close => {
+        const token = this.props.egoToken;
+        const header = {headers: {Authorization: 'Bearer ' + token}};
+        axios
+          .post(`${coordinatorApi}/releases`, release, header)
+          .then(resp => {
+            this.props.history.push(`/releases/${resp.data.kf_id}`);
             close();
-          },
-        });
-      }
+          })
+          .catch(err => {
+            console.log(err);
+            this.setState({
+              loading: false,
+              error: JSON.stringify(err.response.data),
+            });
+            close();
+          });
+      },
+      onCancel(close) {
+        close();
+      },
     });
   };
 
   render() {
     return (
-      <Form model="release" onSubmit={val => this.handleSubmit(val)}>
+      <Form model="releaseForm" onSubmit={val => this.handleSubmit(val)}>
         <label>Release Title:</label>
-        <Control.text model=".title" />
-
+        <Control.text
+          model=".title"
+          defaultValue="Title"
+          validators={{
+            required: val => val && val.length,
+          }}
+        />
+        <Errors
+          className="text-red"
+          model=".title"
+          messages={{
+            required: 'This field is required',
+          }}
+        />
         <label>Is this a major release:</label>
         <Control.checkbox model=".isMajor" /> Yes
-
         <label>Select studies to be included in this release</label>
-        <StudiesContainer selectable />
-
+        <Errors
+          className="text-red"
+          model="studies"
+          messages={{
+            required: 'Select at least one study to release',
+          }}
+        />
+        <Control.custom
+          model="studies"
+          component={StudiesContainer}
+          selectable
+          defaultPageSize={10}
+          validators={{
+            required: val => val.selected.items.length !== 0,
+          }}
+        />
         <label>Services to be run for this release:</label>
         <ServiceList />
-
-        <Button htmlType="submit">Start the Release</Button>
+        <Button type="submit">Start the Release</Button>
       </Form>
     );
   }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+function mapStateToProps(state) {
+  return {
+    studies: state.studies.items,
+  };
 }
 
 function ReleaseProps(props) {
@@ -114,4 +141,7 @@ function ReleaseProps(props) {
   );
 }
 
-export default withRouter(NewReleaseForm);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withRouter(ReleaseProps));
