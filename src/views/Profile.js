8e143467ba8847e8d42d5436f63cd3b1eb6c8d33
Dan Kolbman <dankolbman@gmail.com>
2018-12-18 15:09:58 -0500

    :recycle: Disconnect profile from context

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,33 +1,12 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import { withRouter } from "react-router-dom";
-import axios from 'axios';
 import { Tag } from 'antd';
 import { Card } from 'kf-uikit';
 import { dataserviceApi } from '../globalConfig';
 import { UserContext } from '../contexts';
 
 class Profile extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loading: false,
-		}
-  }
-
-  componentDidMount() {
-    axios.get(`${dataserviceApi}/studies?limit=100`)
-      .then(resp => {
-        let studies = resp.data.results.map((s, i) => ({
-          key: s.kf_id,
-          title: s.kf_id,
-          description: `${s.name}`,
-          chosen: false
-        }));
-        this.setState({data: studies, loading: false, error: '', tags: []});
-      });
-  }
 
   render() {
     const roles = this.props.user.roles.map((t, i) => (
@@ -45,13 +24,15 @@ class Profile extends Component {
   }
 }
 
-function ProfileProps(props) {
-  return (
-    <UserContext.Consumer>
-      {user => <Profile {...props}
-        user={user.user} egoToken={user.egoToken}/>}
-    </UserContext.Consumer>
-  )
-};
+function mapDispatchToProps(dispatch) {
+  return {};
+}
 
-export default withRouter(ProfileProps);
+function mapStateToProps(state) {
+  return {user: state.auth.user};
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withRouter(Profile));
