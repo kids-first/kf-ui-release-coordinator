2e594398077b5de35481852f0ea276c13b4cecf4
Dan Kolbman <dankolbman@gmail.com>
2019-03-24 10:09:55 -0400

    :recycle: Remove ant from login

--- src/views/Login.js
+++ src/views/Login.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Button} from 'kf-uikit';
 import {GoogleLogin} from 'react-google-login';
-import {Row, Col, Layout} from 'antd';
 import {googleAppId} from '../globalConfig';
 import {loginUser} from '../actions/auth';
 import brand from '../brand.svg';
@@ -10,49 +9,40 @@ import brand from '../brand.svg';
 class Login extends Component {
   render() {
     return (
-      <Layout style={{height: '100vh', background: '#2b388f'}}>
-        <Row
-          type="flex"
-          justify="space-around"
-          align="middle"
-          style={{height: '100vh', background: '#2b388f'}}
-        >
-          <Col>
-            <center>
-              <img src={brand} alt="Kids First logo" />
-            </center>
-            <h1 style={{color: '#fff'}}>Kids First Release Coordinator</h1>
-            <br />
-            <center>
-              <GoogleLogin
-                clientId={googleAppId}
-                render={renderProps => (
-                  <Button
-                    size="large"
-                    className="my-2"
-                    onClick={renderProps.onClick}
-                  >
-                    Login with Ego
-                  </Button>
-                )}
-                onSuccess={googleUser => {
-                  const {id_token} = googleUser.getAuthResponse();
-                  this.props.login(id_token);
-                }}
-                onFailure={error => console.log('login fail', error)}
-              />
-              <br />
+      <div className="h-screen bg-blue py-16 text-center">
+        <center>
+          <img src={brand} alt="Kids First logo" />
+        </center>
+        <h1 style={{color: '#fff'}}>Kids First Release Coordinator</h1>
+        <br />
+        <center>
+          <GoogleLogin
+            clientId={googleAppId}
+            render={renderProps => (
               <Button
                 size="large"
                 className="my-2"
-                onClick={() => this.props.auth.login()}
+                onClick={renderProps.onClick}
               >
-                Login with Auth0
+                Login with Ego
               </Button>
-            </center>
-          </Col>
-        </Row>
-      </Layout>
+            )}
+            onSuccess={googleUser => {
+              const {id_token} = googleUser.getAuthResponse();
+              this.props.login(id_token);
+            }}
+            onFailure={error => console.log('login fail', error)}
+          />
+          <br />
+          <Button
+            size="large"
+            className="my-2"
+            onClick={() => this.props.auth.login()}
+          >
+            Login with Auth0
+          </Button>
+        </center>
+      </div>
     );
   }
 }
