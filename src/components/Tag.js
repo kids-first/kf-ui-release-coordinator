fcf1c2f59187b26958a86bd6f3b3b9c90da2cd62
Dan Kolbman <dankolbman@gmail.com>
2019-03-23 22:03:54 -0400

    :recycle: Remove ant from task list

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -4,13 +4,20 @@ import PropTypes from 'prop-types';
 /* The Tag component maps colors to different types of ids
  * to keep the styling consistent throughout the app
  */
-const Tag = ({type, children}) => {
+const Tag = ({type, state, children}) => {
   const style =
     {
       release: 'border-purple',
       task: 'border-orange',
       service: 'border-teal',
-    }[type] || 'border-mediumGrey bg-lightGrey';
+    }[type] || {
+      running: 'border-orange',
+      publishing: 'border-orange',
+      canceled: 'border-pink',
+      failed: 'border-red',
+      published: 'border-teal',
+    }[state] ||
+    'border-mediumGrey bg-lightGrey';
   return (
     <span
       className={`px-2 py-1 m-2 font-medium text-sm cursor-default border-2 rounded-lg ${style}`}
