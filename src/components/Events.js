d426703e4dd25fffa2968750a9ed8de8c1337ece
Dan Kolbman <dankolbman@gmail.com>
2019-03-23 21:35:57 -0400

    :recycle: Remove ant from event list

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,67 +1,42 @@
-import React, {Component} from 'react';
-import {Timeline, Icon, Tag} from 'antd';
+import React from 'react';
+import className from 'classnames';
+import Tag from './Tag';
 
-class Events extends Component {
-  whatColor(ev) {
-    var color = 'green';
-    switch (ev.event_type) {
-      case 'info':
-        color = '#19a9c4';
-        break;
-      case 'warning':
-        color = 'red';
-        break;
-      case 'error':
-        color = 'red';
-        break;
-      default:
-        color = 'blue';
-    }
-    return color;
-  }
-
-  whatIcon(ev) {
-    var icon = 'info';
-
-    return icon;
-  }
-
-  render() {
-    let timeline = this.props.events.map((ev, i) => (
-      <Timeline.Item
-        color={this.whatColor(ev)}
-        dot={
-          <Icon
-            type={this.whatIcon(ev)}
-            style={{fontSize: '18px', color: this.whatColor(ev)}}
-          />
-        }
+const Events = ({events}) => (
+  <ul className="list-reset">
+    {events.map((ev, i) => (
+      <li
         key={i}
+        className={className('p-2 border border-lightGrey', {
+          'bg-lightGrey': i % 2,
+        })}
       >
-        <em>{new Date(ev.created_at).toLocaleString('en-US')}</em>- {ev.message}{' '}
-        <div />
-        {ev.release && <Tag color="blue">{ev.release.slice(1).slice(-11)}</Tag>}
-        {ev.task_service && (
-          <Tag color="orange">{ev.task_service.slice(1).slice(-11)}</Tag>
-        )}
-        {ev.task && <Tag color="magenta">{ev.task.slice(1).slice(-11)}</Tag>}
-      </Timeline.Item>
-    ));
-    return (
-      <Timeline
-        pending={
-          this.props.events.length > 0 &&
-          !this.props.events[this.props.events.length - 1].message.includes(
-            'release published',
-          )
-            ? ''
-            : ''
-        }
-      >
-        {timeline}
-      </Timeline>
-    );
-  }
-}
+        <ul className="m-0 p-1 list-reset inline-block">
+          {ev.release && (
+            <li key={ev.release.kf_id} className="inline-block">
+              <Tag type="release">{ev.release.slice(1).slice(-11)}</Tag>
+            </li>
+          )}
+          {ev.task_service && (
+            <li key={ev.task_service.kf_id} className="inline-block">
+              <Tag type="service">{ev.task_service.slice(1).slice(-11)}</Tag>
+            </li>
+          )}
+          {ev.task && (
+            <li key={ev.task.kf_id} className="inline-block">
+              <Tag type="task">{ev.task.slice(1).slice(-11)}</Tag>
+            </li>
+          )}
+        </ul>
+        <br />
+        <span className="font-semibold">
+          {new Date(ev.created_at).toLocaleString('en-US')}
+        </span>
+        <br />
+        <span className="font-mono">{ev.message}</span>
+      </li>
+    ))}
+  </ul>
+);
 
 export default Events;
