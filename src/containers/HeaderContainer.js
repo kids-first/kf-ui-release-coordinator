fc79effa4e9dcd68c41adcf6abe33670ffd2a00e
Dan Kolbman <dankolbman@gmail.com>
2019-05-30 19:47:08 -0400

    :lipstick: Format header with semantic

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -1,48 +1,45 @@
 import React, {Fragment} from 'react';
-import {NavLink} from 'react-router-dom';
+import {NavLink, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
-import {Header, Button} from 'kf-uikit';
+import {Button, Icon, Image, Menu} from 'semantic-ui-react';
 import {syslevel} from '../globalConfig';
+import logo from '../logo.svg';
+
+const Nav = props => <NavLink exact {...props} activeClassName="active" />;
 
 const HeaderContainer = ({...props}) => {
   return (
     <Fragment>
+      <Menu attached="top">
+        <Menu.Item header as={NavLink} to="/" activeClassName="">
+          <Image src={logo} size="mini" />
+          Release Coordinator
+        </Menu.Item>
+        <Menu.Item as={Nav} to="/" content="Status" />
+        <Menu.Item as={Nav} to="/planner" content="Planner" />
+        <Menu.Item as={Nav} to="/releases" content="Releases" />
+        <Menu.Item as={Nav} to="/studies" content="Studies" />
+        <Menu.Item as={Nav} to="/services" content="Services" />
+        <Menu.Menu position="right">
+          <Menu.Item as={Nav} name={props.userName} to="/profile">
+            <Icon name="user" />
+            {props.userName}
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
       {syslevel !== 'prd' && (
         <center>
           <h3
-            className={`m-0 text-white ${
-              syslevel === 'qa' ? 'bg-orange' : 'bg-pink'
-            }`}
+            style={{
+              color: 'white',
+              background: `${syslevel === 'qa' ? '#FE9A76' : '#008080'}`,
+            }}
           >
             FYI, you're currently in the <b>{syslevel}</b> environment. Anything
             you do here will not be made public!
           </h3>
         </center>
       )}
-      <Header
-        buttons={[
-          <NavLink key="status" to="/">
-            <Button outline>Status</Button>
-          </NavLink>,
-          <NavLink key="planner" to="/planner">
-            <Button outline>Planner</Button>
-          </NavLink>,
-          <NavLink key="releases" to="/releases">
-            <Button outline>Releases</Button>
-          </NavLink>,
-          <NavLink key="studies" to="/studies">
-            <Button outline>Studies</Button>
-          </NavLink>,
-          <NavLink key="services" to="/services">
-            <Button outline>Services</Button>
-          </NavLink>,
-          <NavLink key="profile" to="/profile">
-            <Button outline color="secondary">
-              {props.userName}
-            </Button>
-          </NavLink>,
-        ]}
-      />
     </Fragment>
   );
 };
@@ -55,7 +52,9 @@ function mapStateToProps(state) {
   return {userName: state.auth.user.name};
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(HeaderContainer);
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(HeaderContainer),
+);
