d7c3300bcdcb2b54f33bbea7ed9bb9fac6024844
Dan Kolbman <dankolbman@gmail.com>
2019-09-13 13:22:18 -0400

    :lipstick: Restyle new service form

--- src/forms/NewService.js
+++ src/forms/NewService.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import axios from 'axios';
-import {Button} from 'semantic-ui-react';
+import {Button, Form, Message} from 'semantic-ui-react';
 import {coordinatorApi} from '../globalConfig';
 
 class NewServiceForm extends Component {
@@ -44,28 +44,31 @@ class NewServiceForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Service Name
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Service Endpoint
-          <input type="text" name="url" />
-        </label>
-        <label>
-          Description
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group widths="equal">
+          <Form.Field>
+            <label>Service Name</label>
+            <input type="text" name="name" placeholder="My Service" />
+          </Form.Field>
+          <Form.Field>
+            <label>Service Endpoint</label>
+            <input type="text" name="url" placeholder="http://myservice" />
+          </Form.Field>
+        </Form.Group>
+        <Form.Field>
+          <label>Description</label>
           <input type="text" name="description" />
-        </label>
-        {this.state.error && (
-          <div className="w-full my-2 p-2 bg-lightGrey">
-            <h4 className="m-0">Error :(</h4> <p>{this.state.error}</p>
-          </div>
-        )}
-        <Button color="primary" loading={this.state.loading} type="submit">
+        </Form.Field>
+        {this.state.error && <Message negative content={this.state.error} />}
+        <Button
+          color="primary"
+          loading={this.state.loading}
+          type="submit"
+          floated="right"
+        >
           Register
         </Button>
-      </form>
+      </Form>
     );
   }
 }
