5cfe8df70fd5d8147b95338b436390dc9b2d867c
Dan Kolbman <dankolbman@gmail.com>
2019-09-27 15:58:16 -0400

    :recycle: Refactor LatestPublish to use GraphQL

--- src/queries.js
+++ src/queries.js
@@ -1,5 +1,23 @@
 import {gql} from 'apollo-boost';
 
+export const ALL_RELEASES = gql`
+  query AllReleases($first: Int, $state: String) {
+    allReleases(first: $first, state: $state, orderBy: "-created_at") {
+      edges {
+        node {
+          id
+          kfId
+          version
+          name
+          createdAt
+          isMajor
+          description
+        }
+      }
+    }
+  }
+`;
+
 export const ALL_EVENTS = gql`
   query AllEvents {
     allEvents(orderBy: "-created_at") {
