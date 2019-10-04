81002fd82720b0938c2e7a3ae941d5e312d1c9d3
Dan Kolbman <dankolbman@gmail.com>
2019-10-04 15:20:57 -0400

    :recycle: Refactor service list to use GraphQL

--- src/views/Services.js
+++ src/views/Services.js
@@ -1,69 +1,53 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React from 'react';
+import {useQuery} from '@apollo/react-hooks';
 import {Link} from 'react-router-dom';
-import {withRouter} from 'react-router-dom';
 import {Segment, Button, Card, Grid, Header, Icon} from 'semantic-ui-react';
-import {coordinatorApi} from '../globalConfig';
 import ServiceList from '../components/ServiceList';
 
-class Services extends Component {
-  constructor(props) {
-    super(props);
+import {ALL_SERVICES} from '../queries';
 
-    this.state = {
-      data: [],
-      loading: true,
-    };
-  }
+const Services = props => {
+  const {
+    loading: servicesLoading,
+    error: servicesError,
+    data: services,
+  } = useQuery(ALL_SERVICES);
 
-  componentWillMount() {
-    axios.get(`${coordinatorApi}/task-services`).then(resp => {
-      let data = resp.data.results;
-      this.setState({data: data, loading: false});
-    });
-  }
+  return (
+    <Segment basic>
+      <Card fluid>
+        <Card.Content>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Header as="h2">Kids First Release Task Services</Header>
+              </Grid.Column>
+              <Grid.Column width={8} textAlign="right">
+                <Button
+                  as={Link}
+                  to="/service/new"
+                  primary
+                  size="large"
+                  icon
+                  labelPosition="left"
+                >
+                  <Icon name="settings" />
+                  Register a Service
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Card.Content>
+        <Card.Content extra>
+          <ServiceList
+            services={services && services.allTaskServices.edges}
+            loading={servicesLoading}
+            error={servicesError}
+          />
+        </Card.Content>
+      </Card>
+    </Segment>
+  );
+};
 
-  onChange(state) {
-    console.log(state);
-    axios.post(`${coordinatorApi}/task-services`).then(resp => {
-      let data = resp.data.results;
-      this.setState({data: data, loading: false});
-    });
-  }
-
-  render() {
-    return (
-      <Segment basic>
-        <Card fluid>
-          <Card.Content>
-            <Grid>
-              <Grid.Row>
-                <Grid.Column width={8}>
-                  <Header as="h2">Kids First Release Task Services</Header>
-                </Grid.Column>
-                <Grid.Column width={8} textAlign="right">
-                  <Button
-                    as={Link}
-                    to="/service/new"
-                    primary
-                    size="large"
-                    icon
-                    labelPosition="left"
-                  >
-                    <Icon name="settings" />
-                    Register a Service
-                  </Button>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Card.Content>
-          <Card.Content extra>
-            <ServiceList />
-          </Card.Content>
-        </Card>
-      </Segment>
-    );
-  }
-}
-
-export default withRouter(Services);
+export default Services;
