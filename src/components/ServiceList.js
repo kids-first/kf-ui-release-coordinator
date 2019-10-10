e5a4833aeeac574877fe5eed4f70908f80f5b7e8
Dan Kolbman <dankolbman@gmail.com>
2019-10-10 15:04:05 -0400

    :recycle: Refactor service list to use GrahpQL mutations

--- src/components/ServiceList.js
+++ src/components/ServiceList.js
@@ -1,33 +1,44 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import {Loader, List, Icon, Radio} from 'semantic-ui-react';
+import React, {Fragment, useState} from 'react';
+import {useMutation} from '@apollo/react-hooks';
 import {Link, withRouter} from 'react-router-dom';
-import {coordinatorApi} from '../globalConfig';
+import {Button, Loader, List, Message, Icon, Radio} from 'semantic-ui-react';
+
+import {UPDATE_SERVICE} from '../mutations';
 
 const Service = ({item}) => {
-  const [loading, setLoading] = useState(false);
   const [service, setService] = useState(item);
 
+  const [
+    updateService,
+    {loading: updateServiceLoading, error: updateServiceError},
+  ] = useMutation(UPDATE_SERVICE);
+
   const toggle = ev => {
-    setLoading(true);
-    axios
-      .patch(`${coordinatorApi}/task-services/${service.kfId}`, {
-        enabled: !service.enabled,
-      })
-      .then(resp => {
-        setService(resp.data);
-        setLoading(false);
-      });
+    updateService({
+      variables: {
+        taskService: service.id,
+        input: {
+          name: service.name,
+          url: service.url,
+          enabled: !service.enabled,
+        },
+      },
+    }).then(resp => {
+      setService(resp.data.updateTaskService.taskService);
+    });
   };
 
   return (
     <List.Item>
       <List.Content floated="right">
-        {loading && <Loader active inline size="tiny" />}
+        {updateServiceLoading && <Loader active inline size="tiny" />}
+        {updateServiceError && (
+          <Message header="Error" content={updateServiceError} />
+        )}
         <Radio
           toggle
           label={service.enabled ? 'Enabled' : 'Disabled'}
-          disabled={loading}
+          disabled={updateServiceLoading}
           checked={service.enabled}
           onChange={ev => toggle()}
         />
@@ -48,7 +59,7 @@ const Service = ({item}) => {
 };
 
 const ServiceList = ({services, loading, error}) => {
-  if (loading || !services) {
+  if (loading) {
     return (
       <Loader active inline="centered">
         Loading Services...
@@ -56,11 +67,22 @@ const ServiceList = ({services, loading, error}) => {
     );
   }
   if (error) return <div>Error getting services</div>;
+  if (!services) {
+    return (
+      <Fragment>
+        No Services have been registered yet.
+        <br />
+        <Button as={Link} to="/service/new">
+          Register a Service
+        </Button>
+      </Fragment>
+    );
+  }
 
   return (
     <List>
-      {services.map(service => (
-        <Service item={service} key={service.kfId} />
+      {services.map(({node}) => (
+        <Service item={node} key={node.kfId} />
       ))}
     </List>
   );
