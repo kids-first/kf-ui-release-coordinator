a70c897135b822b51e429d4bddb37aea092db718
Dan Kolbman <dankolbman@gmail.com>
2019-10-15 09:39:23 -0400

    :package: Remove unused packages

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,3 @@
 module.exports = function override(config, env) {
-  require('react-app-rewire-postcss')(config, {
-    plugins: loader => [
-      require('autoprefixer')(),
-      require('postcss-import')(),
-      require('postcss-nested')(),
-      require('postcss-inherit')(),
-    ],
-  });
-
   return config;
 };
