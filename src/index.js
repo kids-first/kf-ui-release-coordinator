09d467d296344bca9e4d49cf69a5683770538219
Dan Kolbman <dankolbman@gmail.com>
2019-10-15 09:18:18 -0400

    :fire: Remove redux

--- src/index.js
+++ src/index.js
@@ -1,35 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
-import {persistStore, persistReducer} from 'redux-persist';
-import {PersistGate} from 'redux-persist/integration/react';
-import storage from 'redux-persist/lib/storage';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import reducer from './reducers';
 import App from './App';
 
-const persistedReducer = persistReducer(
-  {key: 'root', storage, whitelist: ['auth']},
-  reducer,
-);
-
-const store = createStore(
-  persistedReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
-);
-
-const persistor = persistStore(store);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
