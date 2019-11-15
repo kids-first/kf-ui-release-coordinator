21c6e4e83ba63cab5a1e87aae824d15823be1a5f
XuTheBunny <xzhu.fg@gmail.com>
2019-11-15 13:59:35 -0500

    ♻️ Refactor release description markdown editor to call graphql mutation

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,15 +1,13 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import propTypes from 'prop-types';
-import axios from 'axios';
-import {Button, Icon, Segment} from 'semantic-ui-react';
+import {useMutation} from '@apollo/react-hooks';
+import {UPDATE_RELEASE} from '../mutations';
+import {Button, Icon, Segment, Message} from 'semantic-ui-react';
 import ReactMarkdown from 'react-markdown';
-import {coordinatorApi} from '../globalConfig';
-
 import {Editor} from 'react-draft-wysiwyg';
 import {EditorState, convertToRaw, ContentState} from 'draft-js';
 import draftToMarkdown from 'draftjs-to-markdown';
 import '../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-
 /**
  * Displays a release description or note. If there is no description or note,
  * a button prompting for one will be dislpayed, and will show a text area
@@ -22,250 +20,163 @@ import '../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
  * Adding a new note will POST it to /release-notes and store the newly
  * assigned kfId. Future changes to the note will PATCH the existing kfId.
  **/
-class MarkdownEditor extends Component {
-  constructor(props) {
-    super(props);
+const MarkdownEditor = ({releaseId, description}) => {
+  const [updateRelease] = useMutation(UPDATE_RELEASE);
+  console.log(releaseId, description);
+  const editorStyle = {
+    cursor: 'text',
+    maxWidth: '100%',
+    height: 'auto',
+    minHeight: '128px',
+    verticalAlign: 'bottom',
+    transition: 'all .3s, height 0s',
+    overflow: 'auto',
+    resize: 'vertical',
+  };
+  const [editing, setEditing] = useState(false);
+  const [error, setError] = useState(null);
+  const [editorState, setEditorState] = useState(
+    EditorState.createWithContent(
+      ContentState.createFromText(description || ''),
+    ),
+  );
 
-    this.state = {
-      releaseId: this.props.releaseId,
-      studyId: this.props.studyId,
-      noteId: this.props.noteId,
-      description: this.props.description,
-      loading: false,
-      updating: false,
-      toggling: false,
-      saving: false,
-      editorState: EditorState.createWithContent(
-        ContentState.createFromText(this.props.description || ''),
-      ),
-    };
-  }
-
-  /* Saves or creates the description or note */
-  save() {
-    this.setState({saving: true});
-
-    // PATCH the release description
-    if (this.props.type === 'release') {
-      this.updateRelease();
-    } else if (this.props.type === 'release-note') {
-      if (this.state.noteId === null) {
-        this.newNote();
-      } else {
-        this.updateNote();
-      }
-    }
-
-    this.editToggle();
-  }
-
-  /* POSTs a new note for a given study and release */
-  newNote() {
-    const rawState = convertToRaw(this.state.editorState.getCurrentContent());
+  const saveDescription = () => {
+    const rawState = convertToRaw(editorState.getCurrentContent());
     const mdText = draftToMarkdown(rawState);
-    const url = `${coordinatorApi}/release-notes`;
-    axios
-      .post(url, {
-        release: `${coordinatorApi}/releases/${this.state.releaseId}`,
-        study: `${coordinatorApi}/studies/${this.state.studyId}`,
-        description: mdText,
-        author: this.props.user.name,
-      })
+
+    updateRelease({
+      variables: {
+        release: releaseId,
+        input: {
+          description: mdText,
+        },
+      },
+    })
       .then(resp => {
-        console.log(resp);
-        this.setState({saving: false, description: mdText});
+        setError(null);
       })
-      .catch(error => console.log(error));
-  }
-
-  /* PATCHes an existing note's description */
-  updateNote() {
-    const rawState = convertToRaw(this.state.editorState.getCurrentContent());
-    const mdText = draftToMarkdown(rawState);
-    const url = `${coordinatorApi}/release-notes/${this.state.noteId}`;
-    axios
-      .patch(url, {description: mdText})
-      .then(resp => {
-        this.setState({saving: false, description: mdText});
-      })
-      .catch(error => console.log(error));
-  }
-
-  /* PATCHes an existing release's description */
-  updateRelease() {
-    const rawState = convertToRaw(this.state.editorState.getCurrentContent());
-    const mdText = draftToMarkdown(rawState);
-    const url = `${coordinatorApi}/releases/${this.state.releaseId}`;
-    axios
-      .patch(url, {description: mdText})
-      .then(resp => {
-        this.setState({saving: false, description: mdText});
-      })
-      .catch(error => console.log(error));
-  }
-
-  /* Switches between viewing description and editor */
-  editToggle() {
-    if (this.state.editing) {
-      this.setState({editing: false});
-    } else {
-      this.setState({editing: true});
-    }
-  }
-
-  updateDescription(ev) {
-    this.setState({description: ev.target.value});
-  }
-
-  toggle(enabled) {
-    this.setState({toggling: true});
-    axios
-      .patch(`${coordinatorApi}/task-services/${this.state.service.kf_id}`, {
-        enabled: enabled,
-      })
-      .then(resp => {
-        let service = this.state.service;
-        service.enabled = enabled;
-        this.setState({service: service, toggling: false});
+      .catch(err => {
+        setError(err.message);
       });
-  }
-
-  onEditorStateChange: Function = editorState => {
-    this.setState({
-      editorState,
-    });
+    setEditing(false);
   };
 
-  render() {
-    // Saving or loading state
-    if (this.state.loading || this.state.saving) {
-      return (
-        <div className="bg-lightGrey my-32">
-          {this.state.loading ? 'loading...' : 'saving...'}
-        </div>
-      );
-    }
-
-    // Show markdown description or button if no description is available
-    if (!this.state.editing) {
-      if (this.state.description) {
-        return (
-          <>
-            <Segment stacked>
-              <ReactMarkdown source={this.state.description} />
-            </Segment>
-            <Button onClick={() => this.editToggle()}>Edit</Button>
-          </>
-        );
-      } else {
-        return (
-          <center>
-            <Button
-              icon
-              primary
-              labelPosition="left"
-              size="large"
-              onClick={() => this.editToggle()}
-            >
-              <Icon name="pencil" />
-              {this.props.type === 'release'
-                ? ' Add a release summary'
-                : ` Add a new note for ${this.state.studyId}`}
+  return (
+    <>
+      {editing ? (
+        <div>
+          <Segment>
+            <Editor
+              wrapperClassName=""
+              editorClassName=""
+              editorStyle={editorStyle}
+              editorState={editorState}
+              onEditorStateChange={e => setEditorState(e)}
+              toolbar={{
+                options: [
+                  'inline',
+                  'blockType',
+                  'list',
+                  'link',
+                  'emoji',
+                  'image',
+                  'history',
+                ],
+                inline: {
+                  inDropdown: false,
+                  options: ['bold', 'italic'],
+                },
+                blockType: {
+                  inDropdown: true,
+                  options: [
+                    'Normal',
+                    'H1',
+                    'H2',
+                    'H3',
+                    'H4',
+                    'Blockquote',
+                    'Code',
+                  ],
+                },
+                emoji: {
+                  emojis: [
+                    '😀',
+                    '😋',
+                    '😦',
+                    '👍',
+                    '👎',
+                    '👌',
+                    '👻',
+                    '🐛',
+                    '🔥',
+                    '🎉',
+                    '🎊',
+                    '🎁',
+                    '📦',
+                    '📊',
+                    '📈',
+                    '🔇',
+                    '🔈',
+                    '📅',
+                    '✅',
+                    '❎',
+                    '💯',
+                    '❤️',
+                  ],
+                },
+              }}
+            />
+          </Segment>
+          <Segment basic clearing className="noMargin noPadding">
+            <Button floated="right" onClick={() => setEditing(false)}>
+              Cancel
             </Button>
-          </center>
-        );
-      }
-    }
-
-    const editorStyle = {
-      cursor: 'text',
-      maxWidth: '100%',
-      height: 'auto',
-      minHeight: '128px',
-      verticalAlign: 'bottom',
-      transition: 'all .3s, height 0s',
-      overflow: 'auto',
-      resize: 'vertical',
-    };
-
-    const {editorState} = this.state;
-
-    return (
-      <div>
-        <Editor
-          wrapperClassName=""
-          editorClassName=""
-          editorStyle={editorStyle}
-          editorState={editorState}
-          onEditorStateChange={this.onEditorStateChange}
-          toolbar={{
-            options: [
-              'inline',
-              'blockType',
-              'list',
-              'link',
-              'emoji',
-              'image',
-              'history',
-            ],
-            inline: {
-              inDropdown: false,
-              options: ['bold', 'italic'],
-            },
-            blockType: {
-              inDropdown: true,
-              options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'Blockquote', 'Code'],
-            },
-            emoji: {
-              emojis: [
-                '😀',
-                '😋',
-                '😦',
-                '👍',
-                '👎',
-                '👌',
-                '👻',
-                '🐛',
-                '🔥',
-                '🎉',
-                '🎊',
-                '🎁',
-                '📦',
-                '📊',
-                '📈',
-                '🔇',
-                '🔈',
-                '📅',
-                '✅',
-                '❎',
-                '💯',
-                '❤️',
-              ],
-            },
-          }}
-        />
-        <div className="p-4 w-full flex justify-end">
-          <Button secondary onClick={() => this.editToggle()}>
-            Cancel
-          </Button>
-          <Button onClick={() => this.save()}>Save</Button>
+            <Button floated="right" primary onClick={() => saveDescription()}>
+              Save
+            </Button>
+          </Segment>
         </div>
-      </div>
-    );
-  }
-}
+      ) : (
+        <>
+          {description ? (
+            <>
+              <Segment stacked>
+                <ReactMarkdown source={description} />
+              </Segment>
+              <Segment basic clearing className="noMargin noPadding">
+                {error && (
+                  <Message warning compact size="mini" content={error} />
+                )}
+                <Button floated="right" onClick={() => setEditing(true)}>
+                  Edit
+                </Button>
+              </Segment>
+            </>
+          ) : (
+            <Segment basic textAlign="center" className="noMargin noPadding">
+              <Button
+                icon
+                primary
+                labelPosition="left"
+                onClick={() => setEditing(true)}
+              >
+                <Icon name="pencil" />
+                Add a release summary
+              </Button>
+            </Segment>
+          )}
+        </>
+      )}
+    </>
+  );
+};
 MarkdownEditor.propTypes = {
-  releaseId: propTypes.string,
-  studyId: propTypes.string,
-  noteId: propTypes.string,
-  type: propTypes.oneOf(['release', 'release-note']),
+  releaseId: propTypes.string.isRequired,
   description: propTypes.string,
 };
+
 MarkdownEditor.defaultProps = {
-  releaseId: null,
-  studyId: null,
-  noteId: null,
-  type: 'release',
   description: null,
 };
 
