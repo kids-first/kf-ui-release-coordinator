5c020d053f7329c04f8d9e72be4584ee4281882a
XuTheBunny <xzhu.fg@gmail.com>
2019-11-19 10:03:53 -0500

    ✨ Reuse markdown editor component for release note

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,8 +1,21 @@
 import React, {useState} from 'react';
 import propTypes from 'prop-types';
 import {useMutation} from '@apollo/react-hooks';
-import {UPDATE_RELEASE} from '../mutations';
-import {Button, Icon, Segment, Message} from 'semantic-ui-react';
+import {
+  UPDATE_RELEASE,
+  CREATE_RELEASE_NOTE,
+  UPDATE_RELEASE_NOTE,
+  REMOVE_RELEASE_NOTE,
+} from '../mutations';
+import {ALL_NOTES} from '../queries';
+import {
+  Button,
+  Icon,
+  Segment,
+  Message,
+  Popup,
+  Divider,
+} from 'semantic-ui-react';
 import ReactMarkdown from 'react-markdown';
 import {Editor} from 'react-draft-wysiwyg';
 import {EditorState, convertToRaw, ContentState} from 'draft-js';
@@ -20,9 +33,33 @@ import '../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
  * Adding a new note will POST it to /release-notes and store the newly
  * assigned kfId. Future changes to the note will PATCH the existing kfId.
  **/
-const MarkdownEditor = ({releaseId, description}) => {
+const MarkdownEditor = ({releaseId, studyId, releaseNodeId, description}) => {
   const [updateRelease] = useMutation(UPDATE_RELEASE);
-  console.log(releaseId, description);
+  const [createReleaseNote] = useMutation(CREATE_RELEASE_NOTE, {
+    refetchQueries: [
+      {
+        query: ALL_NOTES,
+        variables: {release: releaseId, study: studyId},
+      },
+    ],
+  });
+  const [updateReleaseNote] = useMutation(UPDATE_RELEASE_NOTE, {
+    refetchQueries: [
+      {
+        query: ALL_NOTES,
+        variables: {release: releaseId, study: studyId},
+      },
+    ],
+  });
+  const [removeReleaseNote] = useMutation(REMOVE_RELEASE_NOTE, {
+    refetchQueries: [
+      {
+        query: ALL_NOTES,
+        variables: {release: releaseId, study: studyId},
+      },
+    ],
+  });
+
   const editorStyle = {
     cursor: 'text',
     maxWidth: '100%',
@@ -44,21 +81,55 @@ const MarkdownEditor = ({releaseId, description}) => {
   const saveDescription = () => {
     const rawState = convertToRaw(editorState.getCurrentContent());
     const mdText = draftToMarkdown(rawState);
-
-    updateRelease({
-      variables: {
-        release: releaseId,
-        input: {
-          description: mdText,
+    if (studyId) {
+      if (releaseNodeId) {
+        updateReleaseNote({
+          variables: {
+            releaseNote: releaseNodeId,
+            input: {
+              description: mdText,
+            },
+          },
+        })
+          .then(resp => {
+            setError(null);
+          })
+          .catch(err => {
+            setError(err.message);
+          });
+      } else {
+        createReleaseNote({
+          variables: {
+            input: {
+              study: studyId,
+              release: releaseId,
+              description: mdText,
+            },
+          },
+        })
+          .then(resp => {
+            setError(null);
+          })
+          .catch(err => {
+            setError(err.message);
+          });
+      }
+    } else {
+      updateRelease({
+        variables: {
+          release: releaseId,
+          input: {
+            description: mdText,
+          },
         },
-      },
-    })
-      .then(resp => {
-        setError(null);
       })
-      .catch(err => {
-        setError(err.message);
-      });
+        .then(resp => {
+          setError(null);
+        })
+        .catch(err => {
+          setError(err.message);
+        });
+    }
     setEditing(false);
   };
 
@@ -68,8 +139,6 @@ const MarkdownEditor = ({releaseId, description}) => {
         <div>
           <Segment>
             <Editor
-              wrapperClassName=""
-              editorClassName=""
               editorStyle={editorStyle}
               editorState={editorState}
               onEditorStateChange={e => setEditorState(e)}
@@ -141,14 +210,55 @@ const MarkdownEditor = ({releaseId, description}) => {
         <>
           {description ? (
             <>
-              <Segment stacked>
+              <Segment stacked={studyId == null} basic={studyId != null}>
                 <ReactMarkdown source={description} />
               </Segment>
               <Segment basic clearing className="noMargin noPadding">
                 {error && (
                   <Message warning compact size="mini" content={error} />
                 )}
-                <Button floated="right" onClick={() => setEditing(true)}>
+                {studyId && (
+                  <Popup
+                    trigger={
+                      <Button
+                        negative
+                        floated="right"
+                        onClick={e => e.stopPropagation()}
+                      >
+                        Delete
+                      </Button>
+                    }
+                    header="Are you sure?"
+                    content={
+                      <>
+                        This note will be removed from the study and release
+                        <Divider />
+                        <Button
+                          data-testid="delete-confirm"
+                          negative
+                          fluid
+                          icon={<Icon name="trash alternate" />}
+                          content="Delete"
+                          onClick={e => {
+                            e.stopPropagation();
+                            removeReleaseNote({
+                              variables: {
+                                releaseNote: releaseNodeId,
+                              },
+                            });
+                          }}
+                        />
+                      </>
+                    }
+                    on="click"
+                    position="top right"
+                  />
+                )}
+                <Button
+                  primary
+                  floated="right"
+                  onClick={() => setEditing(true)}
+                >
                   Edit
                 </Button>
               </Segment>
@@ -162,7 +272,7 @@ const MarkdownEditor = ({releaseId, description}) => {
                 onClick={() => setEditing(true)}
               >
                 <Icon name="pencil" />
-                Add a release summary
+                {studyId ? 'Add a release note' : 'Add a release summary'}
               </Button>
             </Segment>
           )}
@@ -173,6 +283,8 @@ const MarkdownEditor = ({releaseId, description}) => {
 };
 MarkdownEditor.propTypes = {
   releaseId: propTypes.string.isRequired,
+  studyId: propTypes.string,
+  releaseNodeId: propTypes.string,
   description: propTypes.string,
 };
 
